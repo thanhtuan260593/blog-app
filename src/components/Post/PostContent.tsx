56e0f817d35f082f5346cdb84d93d642f2735c7e
thanhtuan260593 <thanhtuan12t@gmail.com>
2021-01-25 11:01:26 +0700

    Fix case (#5)
    * temp saved
    
    * fix case

--- src/components/Post/PostContent.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import MarkdownPreview from "@uiw/react-markdown-preview";
-export const PostContent = ({ children }: { children: string }) => {
-  return <MarkdownPreview parserOptions={{}}>{children}</MarkdownPreview>;
-};
